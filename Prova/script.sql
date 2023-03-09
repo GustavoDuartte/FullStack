6ba359d35ff647267c668389ac676760a6153b59
GustavoDuartte <gustavoduartejag@gmail.com>
2023-03-09 11:04:32 -0300

    p

---
+++ Prova/script.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS adamastor;
+
+CREATE DATABASE adamastor;
+
+USE adamastor;
+
+CREATE TABLE
+  veiculo (
+    placa VARCHAR(255) NOT NULL PRIMARY KEY,
+    modelo VARCHAR(255) NOT NULL,
+    marca VARCHAR(255) NOT NULL,
+    cor VARCHAR(255) NOT NULL
+  );
+
+CREATE TABLE
+  cliente (
+    cpf VARCHAR(255) NOT NULL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    rua VARCHAR(255) NOT NULL,
+    bairro VARCHAR(255) NOT NULL,
+    cidade VARCHAR(255) NOT NULL,
+    uf VARCHAR(255) NOT NULL,
+    placa_veiculo VARCHAR(255) NOT NULL,
+    FOREIGN KEY (placa_veiculo) REFERENCES veiculo (placa) ON DELETE CASCADE ON UPDATE CASCADE
+  );
+
+CREATE TABLE
+  telefone (
+    cpf VARCHAR(255) NOT NULL,
+    telefones VARCHAR(255) NOT NULL,
+    FOREIGN KEY (cpf) REFERENCES cliente (cpf) ON DELETE CASCADE ON UPDATE CASCADE
+  );
+
+CREATE TABLE
+  vaga (
+    id INT NOT NULL PRIMARY KEY,
+    tipo VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    valor FLOAT (5, 2) NOT NULL
+  );
+
+CREATE TABLE
+  estacionamento (
+    id INT NOT NULL PRIMARY KEY,
+    entrada DATE NOT NULL,
+    hora_entrada VARCHAR(255) NOT NULL,
+    saida DATE NOT NULL,
+    hora_saida VARCHAR(255) NOT NULL,
+    valor FLOAT (5, 2) NOT NULL,
+    cpf_cliente VARCHAR(255) NOT NULL,
+    id_vaga INT NOT NULL,
+    FOREIGN KEY (id_vaga) REFERENCES vaga (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (cpf_cliente) REFERENCES cliente (cpf) ON DELETE CASCADE ON UPDATE CASCADE
+  );
